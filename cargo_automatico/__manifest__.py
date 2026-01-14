4f2d192ce8f0d41d75132661e37390473f67fbe4
Everth Sanchez <everth.sanz@gmail.com>
2026-01-14 13:50:49 -0600

    Update manifest file for Cargo Automático

---
+++ cargo_automatico/__manifest__.py
@@ -0,0 +1,12 @@
+
+{
+    "name": "Cargo Automático por Mora",
+    "version": "19.0.1.0.0",
+    "license": "LGPL-3",
+    "category": "Accounting",
+    "summary": "Aplica cargos automáticos por mora a facturas vencidas",
+    "depends": ["base", "account"],
+    "data": [],
+    "installable": True,
+    "application": False,
+}
